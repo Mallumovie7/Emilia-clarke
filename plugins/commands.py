4d4055f865e9caea918338fb001db7bcdb13ece0
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-04-28 22:03:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,7 +66,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🔥 ᎫϴᏆΝ ᏟᎻᎪΝΝᎬᏞ 🔥", url=invite_link.invite_link
+                    "🎗️ᎫϴᏆΝ ᏟᎻᎪΝΝᎬᏞ🎗️", url=invite_link.invite_link
                 )
             ]
         ]
@@ -75,7 +75,7 @@ async def start(client, message):
             btn.append([InlineKeyboardButton("🔁 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧 🔁", callback_data=f"checksub#{message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**𝑱𝒐𝒊𝒏 𝑶𝒖𝒓 𝑴𝒐𝒗𝒊𝒆 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 𝑻𝒐 𝑼𝒔𝒆 𝑻𝒉𝒊𝒔 𝑩𝒐𝒕!**",
+            text="**ʕ •ᴥ•ʔ𝐉𝐎𝐈𝐍 𝐌𝐘 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐓𝐎 𝐔𝐒𝐄 𝐌𝐘 𝐁𝐎𝐓😉**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
