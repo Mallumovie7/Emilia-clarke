f238efa128be04b384f452be82324e7e31d481a3
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-04-29 23:18:04 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -60,7 +60,7 @@ async def showid(client, message):
 @Client.on_message(filters.command("about"))
 async def aboutme(client, message):
         buttons= [[
-            InlineKeyboardButton('💖ՏͲᎪͲႮՏ', callback_data='stats')
+            InlineKeyboardButton('💖ՏͲᎪͲႮՏ', callback_data='stats2')
             ],[
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
