0ba67d44866ac7fc829e8fd075969644e1748850
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-04-30 08:43:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -264,18 +264,12 @@ Everyone can use this command , if spaming happens bot will automatically ban yo
 ★ 📥𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
     STATUS2_TXT = """
 
-     █▀ ▀█▀ ▄▀█ ▀█▀ █▀
-
+    █▀ ▀█▀ ▄▀█ ▀█▀ █▀
   ▄█ ░█░ █▀█ ░█░ ▄█
-
 ★ 📁𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-
 ★ 👥𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-
 ★ ❤𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-
 ★ 📤𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-
 ★ 📥𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """ΝᎬᏔᏀᎡϴႮᏢ
     
