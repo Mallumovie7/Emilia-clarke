85997e2bbce862ae06a0a048b5879c8d7d166b86
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-04-30 14:23:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -632,6 +632,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     ],
                     [
                         InlineKeyboardButton('ᏴᎪᏟᏦ', callback_data='photo')
+                    ]
                 ]
             ),
         )
