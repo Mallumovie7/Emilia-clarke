bb0d9848fa2a9242385a1cc45c59fd695ed33d21
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-05-03 21:20:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BalamuruganDV/LUNA-EXTRA-FEATURES.git /LUNA-EXTRA-FEATURES
+  git clone https://github.com/Tanujairam123/emilia-extra-features.git /emilia-extra-features
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LUNA-EXTRA-FEATURES
+  git clone $UPSTREAM_REPO /emilia-extra-features
 fi
-cd /LUNA-EXTRA-FEATURES
+cd /emilia-extra-features
 pip3 install -U -r requirements.txt
-echo "Starting ᎡᏴ[𝙻𝚄𝙽𝙰]💖..."
+echo "𝐒𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝐄𝐦𝐢𝐥𝐢𝐚 𝐜𝐥𝐚𝐫𝐤𝐞 🔥🔥🔥..."
 python3 bot.py
