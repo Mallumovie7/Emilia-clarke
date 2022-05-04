74498f615d3bdec085d59e31f272ca41aa0101ad
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-05-04 11:02:26 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -60,7 +60,7 @@ async def showid(client, message):
 @Client.on_message(filters.command("about"))
 async def aboutme(client, message):
         buttons= [[
-            InlineKeyboardButton('💖ᎠᎬᏙᎬᏞϴᏢᎬᎡ', callback_data='dev')
+            InlineKeyboardButton('👤 ᴏᴛʜᴇʀ ʙᴏᴛs', url='https://t.me/REX_BOTZ/94')
             ],[
             InlineKeyboardButton('🏠 𝙷𝙾𝙼𝙴 🏠', callback_data='start'),
             InlineKeyboardButton('🔐 𝙲𝙻𝙾𝚂𝙴 🔐', callback_data='close_data')
