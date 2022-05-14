4df38166f25f0b6115e71f15dcb2020f5a755611
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2022-05-14 14:08:20 +0530

    Create sticker_id.py

---
+++ plugins/Modules/sticker_id.py
@@ -0,0 +1,8 @@
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command(["stickerid"]))
+async def stickerid(bot, message):   
+    if message.reply_to_message.sticker:
+       await message.reply(f"🆔 Sticker ID is  \n `{message.reply_to_message.sticker.file_id}` \n \n ** 🆔 Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
+    else: 
+       await message.reply("Oops !! Not a sticker file")
