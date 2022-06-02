0ba67bfefda13bd73508076a282992efb2bd885f
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2022-06-02 12:20:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tanujairam-TG/emilia-extra-features.git /emilia-extra-features
+  git clone https://github.com/Tanujairam-TG/Emilia-clarke.git /Emilia-clarke
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /emilia-extra-features
+  git clone $UPSTREAM_REPO /Emilia-clarke
 fi
-cd /emilia-extra-features
+cd /Emilia-clarke
 pip3 install -U -r requirements.txt
 echo "𝐒𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝐄𝐦𝐢𝐥𝐢𝐚 𝐜𝐥𝐚𝐫𝐤𝐞 🔥🔥🔥..."
 python3 bot.py
