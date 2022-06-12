f3bf04a1de10caf6d94ba3a07139826d937dfd80
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2022-06-12 07:23:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        logging.info("𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶 𝙻𝚄𝙽𝙰......")
+        logging.info("Restarting emilia ......")
 
 
 app = Bot()
